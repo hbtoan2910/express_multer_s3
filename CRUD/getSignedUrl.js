4bced7b02348fd251dcb169c457e26e6257aedd1
Toan Huynh <hbtoan2910@gmail.com>
2022-06-03 01:03:01 -0400

    discard .env cause gitignore onot working somehow

--- CRUD/getSignedUrl.js
+++ CRUD/getSignedUrl.js
@@ -4,7 +4,7 @@ function getSignedUrl() {
   var params = {
     Bucket: process.env.S3_BUCKET,
     Key: "1654229517199-2.jpg",
-    Expires: 60,
+    //Expires: 60,
   };
   return s3.getSignedUrl("getObject", params);
 }
